99f11c37b7d9f5ce212ad2d243e918fc9dae1fe6
guylamar2006 <guylovesbritt@gmail.com>
2017-01-17 17:50:17 -0600

    removed libinit_msm

--- init/init_trlte.cpp
+++ init/init_trlte.cpp
@@ -1,6 +1,6 @@
+
 /*
    Copyright (c) 2013, The Linux Foundation. All rights reserved.
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -13,7 +13,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -34,8 +33,9 @@
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+#include <sys/system_properties.h>
 
-#include "init_msm.h"
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
 void gsm_properties()
 {
@@ -43,17 +43,12 @@ void gsm_properties()
     property_set("ro.telephony.default_network", "9");
 }
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
-{
+void init_variant_properties() {
     char platform[PROP_VALUE_MAX];
     char bootloader[PROP_VALUE_MAX];
     char device[PROP_VALUE_MAX];
     char devicename[PROP_VALUE_MAX];
     int rc;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
@@ -63,15 +58,15 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     if (strstr(bootloader, "N910W8")) {
         /* trltecan These values are taken from TMO and edited for the 910W8 FIXME */
-        property_set("ro.build.fingerprint", "samsung/trltevl/trltecan:6.0/MRA58K/N910W8UVU1ANIH:user/release-keys");
-        property_set("ro.build.description", "trltevl-user 6.0 MRA58K N910W8UVU1ANIH release-keys");
+        property_set("ro.build.fingerprint", "samsung/trltevl/trltecan:6.0.1/MRA58K/N910TUVU2EPE3:user/release-keys");
+        property_set("ro.build.description", "trltevl-user 6.0.1 MRA58K N910TUVU2EPE3 release-keys");
         property_set("ro.product.model", "SM-N910W8");
         property_set("ro.product.device", "trltecan");
         gsm_properties();
     } else {
         /* trltetmo */
-        property_set("ro.build.fingerprint", "samsung/trltetmo/trltetmo:6.0/MRA58K/N910TUVU1ANIH:user/release-keys");
-        property_set("ro.build.description", "trltetmo-user 6.0 MRA58K N910TUVU1ANIH release-keys");
+        property_set("ro.build.fingerprint", "samsung/trltetmo/trltetmo:6.0.1/MRA58K/N910TUVU2EPE3:user/release-keys");
+        property_set("ro.build.description", "trltetmo-user 6.0.1 MRA58K N910TUVU2EPE3 release-keys");
         property_set("ro.product.model", "SM-N910T");
         property_set("ro.product.device", "trltetmo");
         gsm_properties();
@@ -80,4 +75,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     property_get("ro.product.device", device);
     strlcpy(devicename, device, sizeof(devicename));
     INFO("Found bootloader id %s setting build properties for %s device\n", bootloader, devicename);
+}
+
+
+void vendor_load_properties() {
+    init_variant_properties();
 }
